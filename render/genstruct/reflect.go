8f51b08ad17b4f5860e83e5733cbd5b40f9c7845
seeflood <zhou.qunli@foxmail.com>
2022-10-13 15:20:43 +0800

    chore: add statement
    Signed-off-by: seeflood <zhou.qunli@foxmail.com>

--- render/genstruct/reflect.go
+++ render/genstruct/reflect.go
@@ -1,3 +1,5 @@
+// The code below is copied from https://github.com/protocolbuffers/protobuf-go
+
 // Copyright 2018 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
